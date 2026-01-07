3b266174565eef9bfe468d6e064d36ab8891e0e3
manuelbc05 <primoahogado@gmail.com>
2026-01-07 16:40:01 +0100

    en el xml de activity main el container view inicie el fragmnt

---
+++ app/src/main/java/com/example/pokemonto/viewmodel/PokemonViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.pokemonto.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.pokemonto.model.PokemonModel
+import com.example.pokemonto.model.PokemonRepository
+
+class PokemonViewModel : ViewModel() {
+    private val _pokemonList = MutableLiveData<List<PokemonModel>>()
+    val pokemonList: LiveData<List<PokemonModel>> = _pokemonList
+
+    init {
+        _pokemonList.value = PokemonRepository.lista
+    }
+}
