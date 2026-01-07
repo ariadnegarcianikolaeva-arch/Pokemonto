bde76be4dc3386e77a58c5bb0dcc77f05562f7b7
manuelbc05 <primoahogado@gmail.com>
2026-01-07 17:51:42 +0100

    preparado pra deslizar

--- app/src/main/java/com/example/pokemonto/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokemonto/viewmodel/PokemonViewModel.kt
@@ -7,10 +7,17 @@ import com.example.pokemonto.model.PokemonModel
 import com.example.pokemonto.model.PokemonRepository
 
 class PokemonViewModel : ViewModel() {
+
     private val _pokemonList = MutableLiveData<List<PokemonModel>>()
     val pokemonList: LiveData<List<PokemonModel>> = _pokemonList
 
     init {
         _pokemonList.value = PokemonRepository.lista
+    }
+
+    fun removePokemon(position: Int) {
+        val currentList = _pokemonList.value?.toMutableList() ?: return
+        currentList.removeAt(position)
+        _pokemonList.value = currentList
     }
 }
