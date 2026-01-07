96c3325c8bfa13ad15af7185dc90d5400990631b
manuelbc05 <primoahogado@gmail.com>
2026-01-07 18:08:23 +0100

    añado un atributo mas, q seria el añadir favoritos

--- app/src/main/java/com/example/pokemonto/model/PokemonModel.kt
+++ app/src/main/java/com/example/pokemonto/model/PokemonModel.kt
@@ -3,5 +3,6 @@ package com.example.pokemonto.model
 data class PokemonModel(
     val nombre: String,
     val descripcion: String,
-    val photo: Int
+    val photo: Int,
+    val favorito: Boolean= false
 )
